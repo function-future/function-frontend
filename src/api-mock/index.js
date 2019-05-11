58caf54465a9ee5ff742dbf48cab6946004a1f68
David William <david_wk98@yahoo.com>
2019-05-11 15:20:54 +0700

    Updated store and configs
    AnnouncementList should now be able to do API call via default-request and use store

--- src/api-mock/index.js
+++ src/api-mock/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
-import { stickyNotes } from '@/api-mock/mock/core-routes'
+import {
+  stickyNotes,
+  announcements,
+  announcementDetails
+} from '@/api-mock/mock/core-routes'
 
 const mock = new MockAdapter(axios)
 
--- src/api-mock/mock/core-routes.js
+++ src/api-mock/mock/core-routes.js
@@ -42,10 +42,27 @@ export const login = [
 ]
 
 //access-list
+export const access = {
+  method: 'GET',
+  url: '/api/core/user/access-list/feeds',
+  response: {
+    "add": true,
+    "delete": true
+  }
+}
 
 //menu-list
-
-//resource-upload
+export const menu = {
+  method: 'GET',
+  url: '/api/core/user/menu-list',
+  response: {
+    "courses": true,
+    "files": true,
+    "users": true,
+    "grades": true,
+    "chatroom": true
+  }
+}
 
 export const stickyNotes = [
   {
@@ -91,8 +108,7 @@ export const announcements = [
   {
     // Get list of announcements
     method: 'GET',
-    url: '/api/core/announcements',
-    param_body: ['page', 'size'],
+    url: '/api/core/announcements?page=1&size=10',
     response: {
       "code": 200,
       "status": "OK",
@@ -167,11 +183,88 @@ export const announcements = [
   {
     //Create new announcement
     method: 'POST',
-    url: '/api/core/announcements',
-    param_body: ['page', 'size'],
+    url: '/api/core/announcements?page=1&size=10',
     response: {
       "code": 201,
       "status": "CREATED",
+      "data": {
+        "id": "sample-id",
+        "title": "Announcement 1",
+        "summary": "Summary goes here. Maximum 70 characters?",
+        "description": "Description goes here. Currently there is no limit to description length.",
+        "files": [
+          {
+            "id": "sample-id",
+            "file": {
+              "full": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_272x92dp.png",
+              "thumbnail": null
+            }
+          }
+        ],
+        "updatedAt": 1555980050616
+      }
+    }
+  }
+]
+
+export const announcementDetails = [
+  {
+    // Get announcement detail
+    method: 'GET',
+    url: '/api/core/announcements/1',
+    response: {
+      "code": 200,
+      "status": "OK",
+      "data": {
+        "id": "sample-id",
+        "title": "Announcement 1",
+        "summary": "Summary goes here. Maximum 70 characters?",
+        "description": "Description goes here. Currently there is no limit to description length.",
+        "files": [
+          {
+            "id": "sample-id",
+            "file": {
+              "full": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_272x92dp.png",
+              "thumbnail": null
+            }
+          }
+        ],
+        "updatedAt": 1555980050616
+      }
+    }
+  },
+  {
+    // Update Announcement
+    method: 'POST',
+    url: '/api/core/announcements/1',
+    response: {
+      "code": 200,
+      "status": "OK",
+      "data": {
+        "id": "sample-id",
+        "title": "Announcement 1",
+        "summary": "Summary goes here. Maximum 70 characters?",
+        "description": "Description goes here. Currently there is no limit to description length.",
+        "files": [
+          {
+            "id": "sample-id",
+            "file": {
+              "full": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_light_color_272x92dp.png",
+              "thumbnail": null
+            }
+          }
+        ],
+        "updatedAt": 1555980050616
+      }
+    }
+  },
+  {
+    // Delete announcement
+    method: 'DELETE',
+    url: '/api/core/announcements/1',
+    response: {
+      "code": 200,
+      "status": "OK",
       "data": {
         "id": "sample-id",
         "title": "Announcement 1",
