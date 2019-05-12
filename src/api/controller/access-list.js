a3f9ff496dce22f9e0f7f7bcff0e6ec4ea97ee01
David William <david_wk98@yahoo.com>
2019-05-12 13:42:08 +0700

    Updated Controller
    MenuList and AccessList is now a separate controller

--- src/api/controller/access-list.js
+++ src/api/controller/access-list.js
@@ -5,11 +5,6 @@ const getAccessList = function (callback, data, errorHandler) {
   request.getRequest(config.api.core.access.accessList(data.url), callback, errorHandler)
 }
 
-const getMenuList = function (callback, errorHandler) {
-  request.getRequest(config.api.core.access.menuList, callback, errorHandler)
-}
-
 export default {
-  getAccessList,
-  getMenuList
+  getAccessList
 }
