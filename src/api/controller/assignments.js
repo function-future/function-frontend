2947405fa16f5270be84495d292343410a6d3e96
David William <david_wk98@yahoo.com>
2019-05-20 01:52:53 +0700

    Added createAssignment

--- src/api/controller/assignments.js
+++ src/api/controller/assignments.js
@@ -5,8 +5,8 @@ const getAssignmentsList = function (callback, data, errorHandler) {
   request.getRequest(config.api.scoring.assignments.list(data.batchCode, data.page, data.pageSize), callback, errorHandler)
 }
 
-const createAssignment = function (callback, data, errorHandler) {
-  request.postRequest(config.api.scoring.assignments.list(data.batchCode, data.page, data.pageSize), callback, errorHandler)
+const createAssignment = function (callback, data, payload, errorHandler) {
+  request.postRequest(config.api.scoring.assignments.create(data.batchCode, data.page, data.pageSize), callback, payload, errorHandler)
 }
 
 export default {
