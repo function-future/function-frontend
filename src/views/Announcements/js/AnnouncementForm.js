9ef304a82c53b65832254dae71683c0a1d404699
karnandohuang <karnandohuang@outlook.com>
2019-05-23 20:27:16 +0700

    reset to initial state when opening announcement form page

--- src/views/Announcements/js/AnnouncementForm.js
+++ src/views/Announcements/js/AnnouncementForm.js
@@ -63,6 +63,7 @@ export default {
       'initialState'
     ]),
     initPage () {
+      this.initialState()
       if (this.editMode) {
         this.getAnnouncementDetail()
         this.setAnnouncementDetail()
