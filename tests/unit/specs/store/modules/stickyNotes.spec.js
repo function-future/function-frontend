bd99085284b389729befb25cce48f4784dd44fa8
David William <david.william@gdn-commerce.com>
2019-05-29 17:55:13 +0700

    Updated stickyNote's store unit test
    Raise stickyNote's vuex coverage to 100%

--- tests/unit/specs/store/modules/stickyNotes.spec.js
+++ tests/unit/specs/store/modules/stickyNotes.spec.js
@@ -8,43 +8,88 @@ describe('actions', () => {
     expect(true).toBe(true)
   })
 
-  // test('fetchStickyNotes', async () => {
-  //   api.getStickyNote = (success) => {
-  //     success({
-  //       'code': 200,
-  //       'status': 'OK',
-  //       'data': [
-  //         {
-  //           'id': 'sample-id',
-  //           'title': 'Sticky Note Title',
-  //           'description': 'Note noteDescription goes here. Length is undetermined.',
-  //           'updatedAt': 1555333551046
-  //         }
-  //       ],
-  //       'paging': {
-  //         'page': 1,
-  //         'size': 1,
-  //         'totalRecords': 100
-  //       }
-  //     })
-  //   }
-  //   const commit = jest.fn()
-  //   const fail = jest.fn()
-  //   store.actions.fetchStickyNotes({commit}, fail)
-  //   expect(fail).toHaveBeenCalledTimes(0)
-  //   expect(commit).toHaveBeenCalledTimes(1)
-  //   expect(commit).toHaveBeenCalledWith('SET_STICKY_NOTES_INFO', [
-  //     {
-  //       'id': 'sample-id',
-  //       'title': 'Sticky Note Title',
-  //       'description': 'Note noteDescription goes here. Length is undetermined.',
-  //       'updatedAt': 1555333551046
-  //     }
-  //   ])
-  // })
-  //
-  // test('postStickyNotes', async () => {
-  //   api.createStickyNote = jest.fn()
-  //   api.createStickyNote.mockResolvedValue()
-  // })
+  test('fetchStickyNotes', async () => {
+    api.getStickyNote = (success) => {
+      success({
+        'code': 200,
+        'status': 'OK',
+        'data': [
+          {
+            'id': 'sample-id',
+            'title': 'Sticky Note Title',
+            'description': 'Note noteDescription goes here. Length is undetermined.',
+            'updatedAt': 1555333551046
+          }
+        ],
+        'paging': {
+          'page': 1,
+          'size': 1,
+          'totalRecords': 100
+        }
+      })
+    }
+    const commit = jest.fn()
+    const fail = jest.fn()
+    store.actions.fetchStickyNotes({ commit }, fail)
+    expect(fail).toHaveBeenCalledTimes(0)
+    expect(commit).toHaveBeenCalledTimes(1)
+    expect(commit).toHaveBeenCalledWith('SET_STICKY_NOTES_INFO', {
+      'noteTitle': 'Sticky Note Title',
+      'noteDescription': 'Note noteDescription goes here. Length is undetermined.',
+      'updatedAt': 1555333551046
+    })
+  })
+
+  test('postStickyNotes', async () => {
+    api.createStickyNote = jest.fn()
+    const data = {
+      'title': 'Sticky Note Title',
+      'description': 'Note description goes here. Length is undetermined.',
+      'updatedAt': 1555333551046
+    }
+    const fail = jest.fn()
+    store.actions.postStickyNotes(data, fail)
+    expect(fail).toBeCalledTimes(0)
+    expect(api.createStickyNote).toBeCalledTimes(1)
+  })
+})
+
+describe('getters', () => {
+  const state = {
+    stickyNote: {
+      noteTitle: '',
+      noteDescription: '',
+      updatedAt: ''
+    }
+  }
+
+  test('stickyNotes', () => {
+    expect(store.getters.stickyNotes(state)).toEqual({
+      noteTitle: state.stickyNote.noteTitle,
+      noteDescription: state.stickyNote.noteDescription,
+      updatedAt: state.stickyNote.updatedAt
+    })
+  })
+})
+
+describe('mutations', () => {
+  const state = {
+    stickyNote: {
+      noteTitle: '',
+      noteDescription: '',
+      updatedAt: ''
+    }
+  }
+  test('SET_STICKY_NOTES_INFO', () => {
+    store.mutations.SET_STICKY_NOTES_INFO(state, {
+      'title': 'Sticky Note Title',
+      'description': 'Note description goes here. Length is undetermined.',
+      'updatedAt': 1555333551046
+    })
+    expect(state.stickyNote).toEqual({
+      'title': 'Sticky Note Title',
+      'description': 'Note description goes here. Length is undetermined.',
+      'updatedAt': 1555333551046
+    })
+  })
 })
