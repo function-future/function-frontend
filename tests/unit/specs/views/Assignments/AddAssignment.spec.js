9584f6fbe35fc490e8393fd1cb51eea35a8938ca
David William <david_wk98@yahoo.com>
2019-06-11 01:32:21 +0700

    Update assignment's coverage
    Added unit test for assignmentController and addAssignment page

---
+++ tests/unit/specs/views/Assignments/AddAssignment.spec.js
@@ -0,0 +1,55 @@
+import addAssignment from '@/views/Assignment/AddAssignment'
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('Assignment', () => {
+  let wrapper
+  let actions
+  let state
+  let store
+
+  beforeEach(() => {
+    state = {
+      assignment: {}
+    }
+    actions = {
+      createAssignment: jest.fn()
+    }
+    store = new Vuex.Store({
+      modules: {
+        assignments: {
+          state,
+          actions
+        }
+      }
+    })
+  })
+
+  test('Sanity test', () => {
+    expect(true).toBe(true)
+  })
+
+  test('Rendered correctly', () => {
+    wrapper = shallowMount(addAssignment, {
+      store,
+      localVue,
+      sync: false
+    })
+    expect(wrapper.isVueInstance()).toBe(true)
+  })
+
+  test('cancel', () => {
+    wrapper = mount(addAssignment, {
+      store,
+      localVue,
+      sync: false
+    })
+    const spy = jest.spyOn(addAssignment.methods, 'cancel')
+    console.log(wrapper.html())
+    wrapper.find('.button-cancel').trigger('click')
+    expect(spy).toBeCalledTimes(1)
+  })
+})
