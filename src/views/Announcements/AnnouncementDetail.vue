3a4e19e140b46419dc843bcd558097bd3cf67076
David William <david_wk98@yahoo.com>
2019-06-14 02:53:37 +0700

    Update unit test coverage
    AnnouncementDetail view's coverage is now at 100%

--- src/views/Announcements/AnnouncementDetail.vue
+++ src/views/Announcements/AnnouncementDetail.vue
@@ -9,8 +9,12 @@
           {{ announcement.updatedAt | moment("dddd, MMMM Do YYYY") }}
         </div>
         <div class="action">
-          <span class="edit-btn" @click="goToEditAnnouncement"><font-awesome-icon icon="edit" class="icon blue" size="lg"></font-awesome-icon></span>
-          <span class="delete-btn" @click="openDeleteConfirmationModal"><font-awesome-icon icon="trash-alt" class="icon red" size="lg"></font-awesome-icon></span>
+          <span class="edit-btn" @click="goToEditAnnouncement">
+            <font-awesome-icon icon="edit" class="icon blue" size="lg"></font-awesome-icon>
+          </span>
+          <span class="delete-btn" @click="openDeleteConfirmationModal">
+            <font-awesome-icon icon="trash-alt" class="icon red" size="lg"></font-awesome-icon>
+          </span>
         </div>
       </div>
       <div class="summary">
