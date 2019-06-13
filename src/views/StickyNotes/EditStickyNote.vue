72a40b2c5e1f522330131e934ac1b0a8534b4d89
David William <david_wk98@yahoo.com>
2019-06-14 01:11:21 +0700

    Update unit test coverage

--- src/views/StickyNotes/EditStickyNote.vue
+++ src/views/StickyNotes/EditStickyNote.vue
@@ -5,7 +5,7 @@
         <BaseInput autofocus
                    class="input-title"
                    inputType="title"
-                   v-model="stickyNotes.noteTitle"
+                   v-model="stickyNote.noteTitle"
                    placeholder="Sticky Note Title"
                    v-validate.disable="'required'"
                    name="title"></BaseInput>
@@ -15,7 +15,7 @@
         <mavon-editor class="editor"
                       placeholder="Sticky Note Description"
                       language="en"
-                      v-model="stickyNotes.noteDescription" v-validate.disable="'required'"
+                      v-model="stickyNote.noteDescription" v-validate.disable="'required'"
                       name="description"></mavon-editor>
       </div>
       <div v-if="errors.has('description')"><span class="input-invalid-message">{{ errors.first('description') }}</span></div>
