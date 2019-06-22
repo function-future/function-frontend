86192d8fabbde0e3a1ef1931d610a08f1a44db40
karnandohuang <karnandohuang@outlook.com>
2019-06-22 14:09:56 +0700

    added CourseCard component

---
+++ src/components/courses/CourseCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <BaseCard cardClass="card-hover">
+    <div class="row">
+      <div class="col basic-info" style="flex-grow: 2">
+        <h3>{{ title }}</h3>
+      </div>
+      <div class="col actions-div" style="flex-grow: 1">
+        <span @click.stop="edit(course.id)"><font-awesome-icon icon="edit" class="icon blue" size="lg"></font-awesome-icon></span>
+        <span @click.stop="deleteCourse(course.id)"><font-awesome-icon icon="trash-alt" class="icon red" size="lg"></font-awesome-icon></span>
+      </div>
+    </div>
+  </BaseCard>
+</template>
+
+<script type="text/javascript" src="./js/course-card.js">
+</script>
+
+<style scoped>
+  .row {
+    display: flex;
+    align-items: center;
+  }
+
+  .col {
+    vertical-align: middle;
+    display: inline-block;
+    padding: 10px;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin: 5px;
+  }
+
+  .basic-info {
+    width: 50%;
+  }
+
+  .future-info {
+    float: right;
+  }
+
+  .actions-div {
+    align-self: center;
+    text-align: center;
+  }
+
+  .actions-div span{
+    padding: 5px;
+    transition: all .2s ease;
+  }
+
+  .actions-div span:hover {
+    opacity: 0.8;
+  }
+
+  .actions-div span:active {
+    opacity: 0.9;
+  }
+</style>
---
+++ src/components/courses/js/course-card.js
@@ -0,0 +1,26 @@
+import BaseCard from '@/components/BaseCard'
+
+export default {
+  name: 'courseCard',
+  components: { BaseCard },
+  props: [
+    'course'
+  ],
+  methods: {
+    edit () {
+      this.$emit('edit', this.course.id)
+    },
+    deleteCourse () {
+      this.$emit('delete', this.course.id)
+    }
+  },
+  computed: {
+    title () {
+      let maximumCharacters = 40
+      if (this.course.title.length > maximumCharacters) {
+        return this.course.title.slice(0, maximumCharacters) + '...'
+      }
+      return this.course.title
+    }
+  }
+}
