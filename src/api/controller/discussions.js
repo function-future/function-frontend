cf8d89379cbcb5ef9c9304def623aaadf5463670
karnandohuang <karnandohuang@outlook.com>
2019-06-23 14:30:36 +0700

    fixed discussions controller

--- src/api/controller/discussions.js
+++ src/api/controller/discussions.js
@@ -2,11 +2,11 @@ import request from '../default-request'
 import config from '@/config/index'
 
 const getCourseDiscussions = function (callback, data, errorHandler) {
-  request.getRequest(config.api.discussions.courses.get(data.batchCode, data.courseId), callback, errorHandler)
+  request.getRequest(config.api.core.discussions.courses.get(data.batchCode, data.courseId), callback, errorHandler)
 }
 
 const postCourseDiscussion = function (callback, data, errorHandler) {
-  request.postRequest(config.api.discussions.courses.get(data.batchCode, data.courseId), callback, errorHandler)
+  request.postRequest(config.api.core.discussions.courses.get(data.batchCode, data.courseId), callback, errorHandler)
 }
 
 export default {
