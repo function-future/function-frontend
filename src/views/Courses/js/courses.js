cd786f87b7085f4c4647039feb3929ecf11aff87
karnandohuang <karnandohuang@outlook.com>
2019-06-30 21:39:01 +0700

    removed console log

--- src/views/Courses/js/courses.js
+++ src/views/Courses/js/courses.js
@@ -162,7 +162,6 @@ export default {
       } else {
         this.selectedIds = []
       }
-      console.log(this.selectedIds)
     }
   }
 }
--- src/views/Courses/js/master-courses.js
+++ src/views/Courses/js/master-courses.js
@@ -153,7 +153,6 @@ export default {
       } else {
         this.selectedIds = []
       }
-      console.log(this.selectedIds)
     }
   }
 }
