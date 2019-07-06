01d52f827879edb7057a57e066c5d9d2b50c8e9a
David William <david_wk98@yahoo.com>
2019-07-06 22:17:02 +0700

    Updated addQuiz
    Added missing unit test for addQuiz that are related to questionBank

---
+++ src/views/Point/Point.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="main-container">
+    <div class="profile-section">
+      <BaseCard :style="{height: '100%'}" class="profile-section__card">
+        <div class="profile-section__card-image">
+          <img src="points.avatar" alt="Image not available"> <!--TODO: CHANGE IMG SRC TO WHATEVER THE API RETURNS !-->
+        </div>
+        <div class="profile-section__card-detail">
+          <h2>{{points.name}}</h2>
+          <span>{{points.university}}</span>
+          <span>{{points.email}}</span>
+        </div>
+      </BaseCard>
+    </div>
+    <div class="points-section">
+      <BaseCard :style="{height: '100%'}" class="point-section__card">
+        <span>Summary</span>
+        <div class="points-section__card-scrollable-content">
+          <BaseCard v-for="point in points.quizzes" class="points-section__card-item" :key="point.id">
+            <div class="points-section__card-item-detail">
+              <h2>{{point.id}}</h2>
+              <span>{{point.title}}</span>
+            </div>
+            <div class="points-section__card-item-score">
+              {{point.point}}
+            </div>
+          </BaseCard>
+        </div>
+      </BaseCard>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript" src="./js/point.js">
+</script>
+
+<style lang="scss" scoped>
+  .main-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 90%;
+
+    .profile-section {
+      height: 20%;
+      &__card {
+        display: flex;
+        flex-direction: row;
+        &-detail {
+          display: flex;
+          flex-direction: column;
+        }
+      }
+    }
+
+    .points-section {
+      height: 70%;
+      &__card {
+        display: flex;
+        flex-direction: column;
+        &-scrollable-content {
+          max-height: 90%;
+          overflow: auto;
+          padding-right: 5px;
+          &::-webkit-scrollbar-track
+          {
+            background-color: #F5F5F5;
+            border-radius: 10px;
+          }
+          &::-webkit-scrollbar
+          {
+            width: 10px;
+            background-color: #02AAF3;
+            border-radius: 10px;
+          }
+          &::-webkit-scrollbar-thumb
+          {
+            border-radius: 10px;
+            background-color: #02AAF3;
+          }
+        }
+        &-item {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          &-detail {
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+          }
+          &-score {
+            align-self: center;
+          }
+        }
+      }
+    }
+  }
+</style>
