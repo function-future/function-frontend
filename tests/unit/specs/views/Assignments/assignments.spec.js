6b470a2a954d48b6448d9406efdd14c62840768e
David William <david_wk98@yahoo.com>
2019-07-10 01:32:59 +0700

    Unit test update
    More to be covered

--- tests/unit/specs/views/Assignments/assignments.spec.js
+++ tests/unit/specs/views/Assignments/assignments.spec.js
@@ -37,7 +37,9 @@ describe('Assignment', () => {
       ]
     }
     const actions = {
-      fetchAssignmentList: jest.fn()
+      fetchAssignmentList: jest.fn(),
+      deleteAssignmentById: jest.fn(),
+      copyAssignment: jest.fn()
     }
     const getters = {
       assignmentList: state => state.assignmentList
@@ -64,7 +66,8 @@ describe('Assignment', () => {
   function createWrapper (store, options) {
     const router = new VueRouter([])
     const $toasted = {
-      error: jest.fn()
+      error: jest.fn(),
+      success: jest.fn()
     }
     return shallowMount(assignment, {
       ...options,
@@ -156,5 +159,112 @@ describe('Assignment', () => {
         batchCode: 3
       }
     })
+  })
+
+  test('openDeleteConfirmationModal', () => {
+    initComponent()
+    wrapper.vm.openDeleteConfirmationModal('sample-id')
+    expect(wrapper.vm.selectedId).toEqual('sample-id')
+    expect(wrapper.vm.showDeleteConfirmationModal).toEqual(true)
+  })
+
+  test('closeDeleteConfirmationModal', () => {
+    initComponent()
+    wrapper.vm.closeDeleteConfirmationModal()
+    expect(wrapper.vm.selectedId).toEqual('')
+    expect(wrapper.vm.showDeleteConfirmationModal).toEqual(false)
+  })
+
+  test('deleteThisAssignment', () => {
+    initComponent()
+    const spy = jest.spyOn(wrapper.vm, 'deleteAssignmentById')
+    wrapper.vm.deleteThisAssignment()
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  test('successDeletingAssignment', () => {
+    initComponent()
+    const routerSpy = jest.spyOn(wrapper.vm.$router, 'push')
+    const closeDeleteConfirmationModal = jest.spyOn(wrapper.vm, 'closeDeleteConfirmationModal')
+    wrapper.vm.successDeletingAssignment()
+    expect(routerSpy).toHaveBeenCalledWith({ name: 'assignments' })
+    expect(wrapper.vm.$toasted.success).toHaveBeenCalledTimes(1)
+    expect(closeDeleteConfirmationModal).toHaveBeenCalledTimes(1)
+  })
+
+  test('failedDeletingAssignment', () => {
+    initComponent()
+    wrapper.vm.failDeletingAssignment()
+    expect(wrapper.vm.$toasted.error).toHaveBeenCalledTimes(1)
+  })
+
+  test('loadPage', () => {
+    initComponent()
+    const spy = jest.spyOn(wrapper.vm, 'initPage')
+    wrapper.vm.loadPage(1)
+    expect(wrapper.vm.paging.page).toEqual(1)
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  test('loadPreviousPage', () => {
+    initComponent()
+    const spy = jest.spyOn(wrapper.vm, 'initPage')
+    wrapper.vm.paging.page = 2
+    wrapper.vm.loadPreviousPage()
+    expect(wrapper.vm.paging.page).toEqual(1)
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  test('loadNextPage', () => {
+    initComponent()
+    const spy = jest.spyOn(wrapper.vm, 'initPage')
+    wrapper.vm.paging.page = 2
+    wrapper.vm.loadNextPage()
+    expect(wrapper.vm.paging.page).toEqual(3)
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  test('openCopyModal', () => {
+    initComponent()
+    wrapper.vm.openCopyModal('sample-id')
+    expect(wrapper.vm.selectedId).toEqual('sample-id')
+    expect(wrapper.vm.showCopyModal).toEqual(true)
+  })
+
+  test('closeCopyModal', () => {
+    initComponent()
+    wrapper.vm.closeCopyModal()
+    expect(wrapper.vm.selectedId).toEqual('')
+    expect(wrapper.vm.showCopyModal).toEqual(false)
+  })
+
+  test('submitCopyModal batchDestination not yet selected', () => {
+    initComponent()
+    const spy = jest.spyOn(wrapper.vm, 'copyAssignment')
+    wrapper.vm.submitCopyModal('')
+    expect(spy).not.toHaveBeenCalled()
+  })
+
+  // test('submitCopyModal have selected batchDestination', () => {
+  //   initComponent()
+  //   console.log(wrapper.vm.AssignmentList)
+  //   const spy = jest.spyOn(wrapper.vm, 'copyAssignment')
+  //   wrapper.vm.submitCopyModal('sample-id')
+  //   expect(spy).toHaveBeenCalledTimes(1)
+  // })
+//  TODO: UNIT TEST THI
+
+  test('successSubmitCopyAssignment', () => {
+    initComponent()
+    wrapper.vm.successSubmitCopyAssignment()
+    expect(wrapper.vm.selectedId).toEqual('')
+    expect(wrapper.vm.showCopyModal).toEqual(false)
+    expect(wrapper.vm.$toasted.success).toHaveBeenCalledTimes(1)
+  })
+
+  test('failSubmitCopyAssignment', () => {
+    initComponent()
+    wrapper.vm.failSubmitCopyAssignment()
+    expect(wrapper.vm.$toasted.error).toHaveBeenCalledTimes(1)
   })
 })
