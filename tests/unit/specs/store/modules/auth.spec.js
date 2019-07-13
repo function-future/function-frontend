788e2d38748d60113907f0c785ec08d0bb8da979
karnandohuang <karnandohuang@outlook.com>
2019-07-13 11:01:32 +0700

    added unit test for accessList and menuList on auth module

--- tests/unit/specs/store/modules/auth.spec.js
+++ tests/unit/specs/store/modules/auth.spec.js
@@ -1,5 +1,6 @@
 import store from '@/store/modules/auth'
 import api from '@/api/controller/auth'
+import configApi from '@/api/controller/config'
 
 jest.mock('@/api/controller/auth')
 
@@ -77,6 +78,47 @@ describe('auth store module', () => {
       expect(callback).toHaveBeenCalledTimes(1)
       expect(fail).not.toHaveBeenCalled()
     })
+
+    test('getMenuList', () => {
+      const expectedData = {
+        'courses': true,
+        'files': true,
+        'users': true,
+        'chatroom': true,
+        'questionBanks': true,
+        'quizzes': true,
+        'assignments': true,
+        'comparisons': true,
+        'points': true
+      }
+      configApi.getMenuList = (success) => {
+        success(expectedData)
+      }
+
+      const commit = jest.fn()
+      store.actions.getMenuList({ commit })
+      expect(commit).toHaveBeenCalledTimes(1)
+    })
+
+    test('getAccessList', () => {
+      const expectedData = {
+        'add': true,
+        'delete': true,
+        'edit': true,
+        'read': true
+      }
+      configApi.getAccessList = (success) => {
+        success(expectedData)
+      }
+
+      const data = '%2Fannouncements'
+      const commit = jest.fn()
+      const callback = jest.fn()
+      const fail = jest.fn()
+      store.actions.getAccessList({ commit }, { data, callback, fail })
+      expect(callback).toHaveBeenCalledTimes(1)
+      expect(fail).not.toHaveBeenCalled()
+    })
   })
 
   describe('courses setter getters', () => {
@@ -92,6 +134,36 @@ describe('auth store module', () => {
         'avatar': 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
       })
       expect(store.getters.currentUser(state)).toEqual(state.currentUser)
+    })
+
+    test('SET_MENU_LIST & menuList getters', () => {
+      store.mutations.SET_MENU_LIST(state, {
+        'courses': true,
+        'files': true,
+        'users': true,
+        'chatroom': true,
+        'questionBanks': true,
+        'quizzes': true,
+        'assignments': true,
+        'comparisons': true,
+        'points': true
+      })
+      expect(store.getters.menuList(state)).toEqual(state.menuList)
+    })
+
+    test('SET_ACCESS_LIST & accessList getters', () => {
+      store.mutations.SET_ACCESS_LIST(state, {
+        'courses': true,
+        'files': true,
+        'users': true,
+        'chatroom': true,
+        'questionBanks': true,
+        'quizzes': true,
+        'assignments': true,
+        'comparisons': true,
+        'points': true
+      })
+      expect(store.getters.accessList(state)).toEqual(state.accessList)
     })
   })
 })
