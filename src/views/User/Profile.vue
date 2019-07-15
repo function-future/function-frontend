5cb1d68036686f8931f08c5502c3cee451c7eda8
Karnando <karnando.sepryan@gdn-commerce.com>
2019-07-15 17:57:26 +0700

    added profile page

---
+++ src/views/User/Profile.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="scrollable-container">
+    <div class="form-container">
+      <div class="row">
+        <div class="column image-column">
+          <div class="image" :style="{ backgroundImage: 'url(' + avatarPreview + ')' }">
+            <!--<input type="file"-->
+                   <!--name="image"-->
+                   <!--accept="image/*"-->
+                   <!--id="upload-image"-->
+                   <!--@change="onFileChange($event)"-->
+                   <!--style="display: none"/>-->
+            <!--<label for="upload-image" class="image-edit">-->
+              <!--<font-awesome-icon icon="pencil-alt" class="icon"/> edit-->
+            <!--</label>-->
+          </div>
+          <div class="alert" v-if="maximumSizeAlert">
+            <span>Please upload a picture smaller than 1 MB.</span>
+          </div>
+        </div>
+        <div class="column input-column">
+          <div class="input-wrapper">
+            <div class="input-label inline">Name</div>
+            <div class="input inline">
+              <BaseInput v-model="profile.name" disabled>
+              </BaseInput>
+            </div>
+          </div>
+          <div class="input-wrapper">
+            <div class="input-label inline">Phone</div>
+            <div class="input inline">
+              <BaseInput v-model="profile.phone" disabled></BaseInput>
+            </div>
+          </div>
+          <div class="input-wrapper">
+            <div class="input-label inline">Email</div>
+            <div class="input inline">
+              <BaseInput v-model="profile.email" disabled></BaseInput>
+            </div>
+          </div>
+          <div class="input-wrapper" v-if="profile.role === 'STUDENT'">
+            <div class="input-label inline">University</div>
+            <div class="input inline">
+              <BaseInput v-model="profile.university" disabled></BaseInput>
+            </div>
+          </div>
+          <div class="input-wrapper" v-if="profile.role !== 'STUDENT'">
+            <div class="input-label inline">Role</div>
+            <div class="input inline">
+              <BaseSelect v-model="profile.role" disabled></BaseSelect>
+            </div>
+          </div>
+          <div class="input-wrapper">
+            <div class="input-label inline">Address</div>
+            <div class="input inline">
+              <BaseInput v-model="profile.address" disabled></BaseInput>
+            </div>
+          </div>
+          <div class="input-wrapper" v-if="profile.role === 'STUDENT'">
+            <div class="input-label inline">Batch</div>
+            <div class="input inline">
+              <BaseInput v-model="profile.batch.code" disabled></BaseInput>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript" src="js/profile.js"></script>
+
+<style lang="scss" scoped>
+  .form-container {
+    margin: 10px;
+  }
+
+  .row {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%;
+  }
+
+  .column {
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+  }
+
+  .image-column {
+    padding: 20px 10px;
+    width: 15vw;
+  }
+
+  .image-title {
+    text-align: left;
+  }
+
+  .image {
+    width: 100%;
+    border-radius: 10px;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
+    padding-top: 100%;
+    position: relative;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+
+  .image-edit {
+    cursor: pointer;
+    background-color: #828282;
+    color: #FFFFFF;
+    padding: 5px 10px;
+    border-radius: 5px;
+    opacity: 0.9;
+    position: absolute;
+    right: 10px;
+    bottom: 10px;
+  }
+
+  .input-column {
+    flex: 2
+  }
+
+  .input-label {
+    width: 15%;
+    text-align: left;
+  }
+
+  .input-wrapper {
+    text-align: right;
+    width: 100%;
+  }
+
+  .input {
+    width: 80%;
+  }
+
+  .inline {
+    display: inline-block;
+  }
+
+  .alert {
+    margin-top: 10px;
+    text-align: left;
+    color: #cb2431 !important;
+  }
+</style>
---
+++ src/views/User/js/profile.js
@@ -0,0 +1,47 @@
+import { mapActions, mapGetters } from 'vuex'
+import BaseInput from '@/components/BaseInput'
+import BaseButton from '@/components/BaseButton'
+import BaseTextArea from '@/components/BaseTextArea'
+import BaseSelect from '@/components/BaseSelect'
+
+export default {
+  name: 'profile',
+  components: {
+    BaseButton,
+    BaseInput,
+    BaseTextArea,
+    BaseSelect
+  },
+  data () {
+    return {
+      maximumSizeAlert: false,
+      avatarPreview: '',
+      userDetail: {}
+    }
+  },
+  created () {
+    this.initPage()
+  },
+  computed: {
+    ...mapGetters([
+      'profile'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'fetchProfile'
+    ]),
+    initPage () {
+      this.fetchProfile({
+        callback: this.successFetchProfile,
+        fail: this.failFetchProfile
+      })
+    },
+    successFetchProfile () {
+      this.avatarPreview = this.profile.avatar
+    },
+    failFetchProfile () {
+      this.$toasted.error('Fail to load profile')
+    }
+  }
+}
