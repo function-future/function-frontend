3316b8d39966998da370aa874252d6a946da17fd
karnandohuang <karnandohuang@outlook.com>
2019-07-16 20:16:46 +0700

    fixed wrong sticky notes mapping

--- src/views/StickyNotes/StickyNotesDetail.vue
+++ src/views/StickyNotes/StickyNotesDetail.vue
@@ -2,11 +2,11 @@
   <div class="scrollable-container">
     <BaseCard class="card" cardClass="card-hover">
       <div class="header">
-        <h3 class="title">{{ stickyNotes.title || 'Sticky Note' }}</h3>
+        <h3 class="title">{{ stickyNotes[0].title || 'Sticky Note' }}</h3>
       </div>
       <div class="header float-right">
         <div class="date">
-          {{ stickyNotes.updatedAt | moment("dddd, MMMM Do YYYY") }}
+          {{ stickyNotes[0].updatedAt | moment("dddd, MMMM Do YYYY") }}
         </div>
         <div class="action">
           <span class="add-btn" @click="goToAddStickyNote" v-if="accessList.edit">
