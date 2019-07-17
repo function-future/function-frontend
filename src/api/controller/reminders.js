1e852cf18ad0d8a09d3721a639231435692b4b82
Priagung Satyagama <priagungsatyagama@gmail.com>
2019-07-17 10:33:58 +0700

    add controller for notification and reminder

---
+++ src/api/controller/reminders.js
@@ -0,0 +1,52 @@
+import request from '../default-request'
+import config from '../../config/index'
+
+const getReminders = function (callback, error, data) {
+  request.getRequest(
+    config.api.communication.reminders.list(data.params.page, config.dev.defaultPageSize),
+    callback,
+    error
+  )
+}
+
+const createReminder = function (callback, error, data) {
+  request.postRequest(
+    config.api.communication.reminders.create,
+    callback,
+    data.body,
+    error
+  )
+}
+
+const getReminder = function (callback, error, data) {
+  request.getRequest(
+    config.api.communication.reminders.get(data.params.reminderId),
+    callback,
+    error
+  )
+}
+
+const updateReminder = function (callback, error, data) {
+  request.putRequest(
+    config.api.communication.reminders.update(data.params.reminderId),
+    callback,
+    data.body,
+    error
+  )
+}
+
+const deleteReminder = function (callback, error, data) {
+  request.deleteRequest(
+    config.api.communication.reminders.delete(data.params.reminderId),
+    callback,
+    error
+  )
+}
+
+export default {
+  getReminder,
+  getReminders,
+  updateReminder,
+  deleteReminder,
+  createReminder
+}
