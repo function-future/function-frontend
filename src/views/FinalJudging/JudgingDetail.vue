aa93aa2b57a658eb5f88ac6cc2c705b8a1f1f2e5
David William <david_wk98@yahoo.com>
2019-07-20 15:15:39 +0700

    Updated JudgingList
    Judging detail progress, still WIP

--- src/views/FinalJudging/JudgingDetail.vue
+++ src/views/FinalJudging/JudgingDetail.vue
@@ -6,7 +6,23 @@
     </div>
     <div class="judging-detail__container-body">
       <!--Body-->
-      <BaseTextArea placeholder="Input description here" :style="{'height': '100%'}"></BaseTextArea>
+      <BaseTextArea class="judging-detail__container-body-description" placeholder="Input description here" :style="{'height': '100%'}"></BaseTextArea>
+      <BaseCard v-if="isLoading" class="judging-detail__container-body-student-list" :style="{'margin': '10px 0 10px 15px'}">
+        <div class="judging-detail__container-body-student-list__header">
+          <div class="judging-detail__container-body-student-list__header-title">
+            Students
+          </div>
+          <div class="judging-detail__container-body-student-list__header-button">
+            <font-awesome-icon icon="edit" class="icon blue" size="lg"></font-awesome-icon>
+            <!--Style this ^^-->
+          </div>
+        </div>
+        <div class="judging-detail__container-body-student-list__content">
+          <BaseCard v-for="student in studentId">
+<!--Make template and logic-->
+          </BaseCard>
+        </div>
+      </BaseCard>
     </div>
     <div class="judging-detail__container-action">
       <!--Action-->
@@ -34,6 +50,15 @@
     &-body {
       min-height: 55vh;
       padding: 10px 0 20px 0;
+      display: flex;
+      justify-content: space-between;
+      &-description {
+        flex: 1;
+      }
+      &-student-list {
+        width: 20%;
+        height: 100%;
+      }
     }
     &-action {
       margin-top: 20px;
