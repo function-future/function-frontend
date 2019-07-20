3a91a423a9133c285458907d25f6369454b206da
karnandohuang <karnandohuang@outlook.com>
2019-07-20 15:50:01 +0700

    added create folder modal

---
+++ src/components/modals/js/modal-create-folder.js
@@ -0,0 +1,34 @@
+import BaseButton from '@/components/BaseButton'
+import BaseInput from '@/components/BaseInput.vue'
+
+export default {
+  name: 'modal-create-folder',
+  components: {
+    BaseButton,
+    BaseInput
+  },
+  data () {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    close () {
+      this.$emit('close')
+    },
+    validateBeforeSubmit (callback) {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          callback()
+        }
+      })
+    },
+    create () {
+      this.validateBeforeSubmit(this.validationSuccess)
+    },
+    validationSuccess () {
+      this.title = ''
+      this.$emit('create')
+    }
+  }
+}
