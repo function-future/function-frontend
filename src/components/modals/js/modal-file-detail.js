d65973a9628acffdd5df5e3e727589e4855c4211
Karnando <karnando.sepryan@gdn-commerce.com>
2019-07-29 15:56:02 +0700

    fixed constrcutFormData

--- src/components/modals/js/modal-file-detail.js
+++ src/components/modals/js/modal-file-detail.js
@@ -83,20 +83,23 @@ export default {
       this.file = e.target.files[0]
       this.upload(this.file)
     },
-    constructFormData (file, type) {
+    constructFormData (file, type, isRenameMode) {
       let data = JSON.stringify({
         name: this.fileDetail.name,
         type: type
       })
       let formData = new FormData()
-      formData.append(data, file)
+      formData.append('data', data)
+      type === 'FOLDER' || isRenameMode
+        ? formData.append('file', '')
+        : formData.append('file', file)
       return formData
     },
     upload (file) {
       let data = {
         parentId: this.$route.params.parentId,
         id: this.fileDetail.id,
-        content: this.constructFormData(file, 'FILE')
+        content: this.constructFormData(file, 'FILE', true)
       }
       let config = {
         headers: { 'Content-Type': 'multipart/form-data' },
