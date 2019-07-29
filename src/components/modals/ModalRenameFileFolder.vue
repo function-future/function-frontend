928c69a9494b250815bef2fc2e334d759deb49d2
Karnando <karnando.sepryan@gdn-commerce.com>
2019-07-29 14:29:15 +0700

    added modal rename file folder

---
+++ src/components/modals/ModalRenameFileFolder.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="modal__mask">
+    <div class="modal__wrapper">
+      <div class="modal__container">
+        <div class="modal__header">
+          <h3 class="modal__header__title">Rename</h3>
+          <span class="modal__close"><font-awesome-icon icon="times" class="icon" @click="close" size="lg"></font-awesome-icon></span>
+        </div>
+        <div class="modal__body">
+          <BaseInput autofocus
+                     class="input-title"
+                     inputType="title"
+                     v-model="title"
+                     placeholder="Untitled Folder"
+                     v-validate.disable="'required'"
+                     name="title">
+          </BaseInput>
+          <div v-if="errors.has('title')">
+            <span class="input-invalid-message">{{ errors.first('title') }}</span>
+          </div>
+        </div>
+        <div class="modal__footer">
+          <BaseButton class="modal__footer__button" type="cancel" buttonClass="button-cancel" @click="close">Cancel</BaseButton>
+          <BaseButton class="modal__footer__button" type="submit" buttonClass="button-save" @click="create">Rename</BaseButton>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript" src="./js/modal-rename-file-folder.js"></script>
+
+<style lang="scss" scoped>
+  .modal {
+    &__mask {
+      position: fixed;
+      z-index: 9999;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, .5);
+      display: table;
+      transition: opacity .3s ease;
+    }
+
+    &__wrapper {
+      display: table-cell;
+      padding-top: 25vh;
+    }
+
+    &__container {
+      display: flex;
+      flex-direction: column;
+      width: 32vw;
+      margin: 0 auto;
+      padding: 10px 20px;
+      background-color: #fff;
+      border-radius: 20px;
+      box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+      transition: all .3s ease;
+      font-family: Helvetica, Arial, sans-serif;
+    }
+
+    &__header {
+      display: flex;
+      align-items: flex-start;
+      margin-top: 0.5rem;
+
+      &__title {
+        padding: 0.3rem;
+        margin: 0;
+        text-align: left;
+        font-weight: bold;
+        font-size: 1.5em;
+      }
+    }
+
+    &__body {
+      margin: 10px 10px 25px 10px;
+      text-align: left;
+    }
+
+    &__footer {
+      margin-bottom: 0.5rem;
+      text-align: left;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+
+      &__button {
+        margin: 0.25rem;
+      }
+    }
+
+    &__close {
+      margin: 0 0 0 auto;
+      padding: 0.5rem;
+      top: 0;
+      float: right;
+      cursor: pointer;
+      transition: all .2s ease;
+
+      &:hover {
+        opacity: 0.8;
+      }
+      &:active {
+        opacity: 0.8;
+      }
+    }
+  }
+
+  .input-invalid-message {
+    color: #FF0000;
+    font-size: 0.75em;
+    float: left;
+    margin-left: 2vw;
+  }
+
+  .modal-enter {
+    opacity: 0;
+  }
+
+  modal-enter-active, .modal-leave-active {
+    opacity: 0;
+  }
+
+  .modal-enter .modal-container,
+  .modal-leave-active .modal-container {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+  }
+</style>
