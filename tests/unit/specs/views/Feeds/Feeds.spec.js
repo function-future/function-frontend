2778c565a78a329a11f099e9a647f95c2c3c5d45
Karnando <karnando.sepryan@gdn-commerce.com>
2019-07-30 10:37:42 +0700

    added more unit tests

--- tests/unit/specs/views/Feeds/Feeds.spec.js
+++ tests/unit/specs/views/Feeds/Feeds.spec.js
@@ -180,4 +180,11 @@ describe('Feeds', () => {
     initComponent()
     expect(wrapper.vm.stickyNotesDescriptionPreview('Note for testing purpose')).toEqual('Note for testing purpose')
   })
+
+  test('stickyNotesDescriptionPreview > max length', () => {
+    const text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n'
+    const textPreview = text.substr(0, 200) + '...'
+    initComponent()
+    expect(wrapper.vm.stickyNotesDescriptionPreview(text)).toEqual(textPreview)
+  })
 })
