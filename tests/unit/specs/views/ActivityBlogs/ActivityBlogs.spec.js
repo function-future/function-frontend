fb70318cb4198997130583f922e962787616fbe2
Karnando <karnando.sepryan@gdn-commerce.com>
2019-07-31 14:35:56 +0700

    added unit tests

--- tests/unit/specs/views/ActivityBlogs/ActivityBlogs.spec.js
+++ tests/unit/specs/views/ActivityBlogs/ActivityBlogs.spec.js
@@ -112,6 +112,11 @@ describe('ActivityBlogs', () => {
     expect(wrapper.vm.compileToMarkdown('hello')).toEqual('<p>hello</p>\n')
   })
 
+  test('showLimitedPreviewText > 350 characters', () => {
+    const text = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
+    expect(wrapper.vm.showLimitedPreviewText(text)).toEqual(text.slice(0, 350) + '...')
+  })
+
   test('goToActivityBlogDetail', () => {
     const push = jest.fn()
     wrapper.vm.$router.push = push
