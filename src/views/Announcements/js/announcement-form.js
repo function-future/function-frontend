2981f944a62933996c790e9fa864aec3544fe8ea
karnandohuang <karnandohuang@outlook.com>
2019-08-07 23:14:00 +0700

    [BUGFIX-16] When uploading image on announcements, submit button should be disabled

--- src/views/Announcements/js/announcement-form.js
+++ src/views/Announcements/js/announcement-form.js
@@ -125,6 +125,7 @@ export default {
       this.$router.push({ name: 'announcements' })
     },
     $imgAdd (pos, $file) {
+      this.uploadingFile = true
       this.pos = pos
       let data = new FormData()
       data.append('file', $file)
@@ -139,6 +140,7 @@ export default {
       })
     },
     successUploadResource (response) {
+      this.uploadingFile = false
       this.$refs.md.$img2Url(this.pos, response.file.full)
       this.imageIds.push(response.id)
     },
