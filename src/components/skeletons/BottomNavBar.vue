ca519fb4e358bd0193e59017f6e283015ed6ac4b
karnandohuang <karnandohuang@outlook.com>
2019-10-13 15:39:29 +0700

    simplify html tags

--- src/components/skeletons/BottomNavBar.vue
+++ src/components/skeletons/BottomNavBar.vue
@@ -1,37 +1,27 @@
 <template>
   <nav class="is-primary navbar is-link is-fixed-bottom has-shadow" role="navigation">
     <div class="navbar-brand">
-      <a class="navbar-item is-expanded is-block has-text-centered">
-        <router-link :to="{ name: 'feeds' }" class="mobile-router-link">
+      <router-link :to="{ name: 'feeds' }" class="navbar-item is-expanded is-block has-text-centered mobile-router-link" exact>
           <b-icon icon="home"></b-icon>
           <div class="is-size-7 menu-title">Home</div>
-        </router-link>
-      </a>
-      <a class="navbar-item is-expanded is-block has-text-centered">
-        <router-link :to="{ name: 'feeds' }" class="mobile-router-link">
-          <b-icon icon="tasks"></b-icon>
-          <p class="is-size-7 menu-title">Scoring</p>
-        </router-link>
-      </a>
-      <a class="navbar-item is-expanded is-block has-text-centered">
-        <router-link :to="{ name: 'chatrooms' }" class="mobile-router-link">
-          <b-icon icon="comments"></b-icon>
-          <p class="is-size-7 menu-title">Chatroom</p>
-        </router-link>
-      </a>
-      <a class="navbar-item is-expanded is-block has-text-centered">
-        <router-link :to="{ name: 'feeds' }" class="mobile-router-link">
-          <b-icon icon="star"></b-icon>
-          <p class="is-size-7 menu-title">PA</p>
-        </router-link>
-      </a>
-      <a class="navbar-item is-expanded is-block has-text-centered">
-        <router-link :to="{ name: 'profile' }" class="mobile-router-link">
-          <!--TODO set this to account page that has list option: login, profile, change password-->
-          <b-icon icon="user"></b-icon>
-          <p class="is-size-7 menu-title">Account</p>
-        </router-link>
-      </a>
+      </router-link>
+      <router-link :to="{ name: 'points' }" class="navbar-item is-expanded is-block has-text-centered mobile-router-link">
+        <b-icon icon="tasks"></b-icon>
+        <p class="is-size-7 menu-title">Scoring</p>
+      </router-link>
+      <router-link :to="{ name: 'chatrooms' }" class="navbar-item is-expanded is-block has-text-centered mobile-router-link">
+        <b-icon icon="comments"></b-icon>
+        <p class="is-size-7 menu-title">Chatroom</p>
+      </router-link>
+      <router-link :to="{ name: 'loggingRoom' }" class="navbar-item is-expanded is-block has-text-centered mobile-router-link">
+        <b-icon icon="star"></b-icon>
+        <p class="is-size-7 menu-title">PA</p>
+      </router-link>
+      <router-link :to="{ name: 'profile' }" class="navbar-item is-expanded is-block has-text-centered mobile-router-link">
+        <!--TODO set this to account page that has list option: login, profile, change password-->
+        <b-icon icon="user"></b-icon>
+        <p class="is-size-7 menu-title">Account</p>
+      </router-link>
     </div>
   </nav>
 </template>
