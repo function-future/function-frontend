5bcedefafb06d0474042cdfc43167dbbabe0c119
karnandohuang <karnandohuang@outlook.com>
2019-10-26 17:01:19 +0700

    removed unused import from edit sticky note js

--- src/views/StickyNotes/js/edit-sticky-note.js
+++ src/views/StickyNotes/js/edit-sticky-note.js
@@ -1,15 +1,6 @@
 import { mapActions, mapGetters } from 'vuex'
-import BaseInput from '@/components/BaseInput'
-import BaseButton from '@/components/BaseButton'
-import BaseTextArea from '@/components/BaseTextArea'
-import config from '@/config/index'
 
 export default {
-  components: {
-    BaseButton,
-    BaseInput,
-    BaseTextArea
-  },
   data () {
     return {
       toolbars: {
