5896a787b564163e08076460911e9c90f3191c5f
Karnando <karnando.sepryan@gdn-commerce.com>
2019-11-04 11:13:31 +0700

    added modal file version with unit test

---
+++ src/components/modals/ModalFileVersion.vue
@@ -0,0 +1,231 @@
+<template>
+  <transition name="modal" appear appear-active-class="fade-enter-active">
+    <div class="modal__mask">
+      <div class="modal__wrapper" @click.self="close">
+        <div class="modal__container">
+          <div class="modal__header">
+            <h3 class="modal__header__title">File Version</h3>
+            <span class="modal__close" @click="close"><b-icon icon="times"></b-icon></span>
+          </div>
+          <div class="modal__body">
+            <div class="columns is-multiline" v-if="isLoading">
+              <div class="column is-12" v-for="n in 3" :key="n">
+                <ListItem :minHeight="'50px'" :simple="true" :loading="isLoading"></ListItem>
+              </div>
+            </div>
+            <div v-if="!isLoading">
+              <div class="modal__body-author">
+                Uploaded by <b>{{ author }}</b>
+              </div>
+              <div class="columns is-mobile"
+                   v-for="(value, name) in fileDetail.versions"
+                   :key="name">
+                <div class="column">
+                  <ListItem :minHeight="'50px'">
+                    <template #content>
+                      <div class="modal__body__file-version__content">
+                        <div class="modal__body__file-version__content-info">
+                        <span class="has-text-weight-bold is-size-4 modal__body__file-version__content-info-number">
+                          {{ name }}
+                        </span>
+                          <span>
+                          {{ value.timestamp | moment("MMMM Do, YYYY") }}
+                        </span>
+                        </div>
+                        <a class="modal__body__file-version__content-link"
+                           :href="value.url" target="_blank" style="color: #4a4a4a">
+                          <b-icon icon="download" size="is-small"></b-icon>
+                        </a>
+                      </div>
+                    </template>
+                  </ListItem>
+                </div>
+              </div>
+            </div>
+            <div class="columns is-mobile is-vcentered" v-if="!fileDetail.versions && !isLoading">
+              <div class="column has-text-centered modal__body__empty-list">
+                No versions available
+              </div>
+            </div>
+          </div>
+          <div class="modal__footer">
+            <b-button class="modal__footer__button" type="is-light" @click="close" expanded>Cancel</b-button>
+            <label class="modal__footer__button button is-primary is-fullwidth"
+                   :class="{'is-loading': isUploading }"
+                   :disabled="disableUpload"
+                   v-if="!isLoading && accessList.edit && (ownerOfTheFile || currentUser.role === 'ADMIN') && !disableUpload"
+                   expanded>
+              <input type="file" @change="onFileChange($event)" v-if="!isUploading && !disableUpload" style="display: none">
+              Upload New Version
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script src="./js/modal-file-version.js"></script>
+
+<style lang="scss" scoped>
+  @import "@/assets/css/main.scss";
+
+  .modal {
+    &__mask {
+      position: fixed;
+      z-index: 9998;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(10, 10, 10, 0.86);
+      display: table;
+      transition: opacity .3s ease;
+    }
+
+    &__wrapper {
+      padding-top: 30vh;
+
+      @media (max-width: 1023px) {
+        height: 100%;
+        display: flex;
+        flex-direction: column-reverse;
+        transition: opacity 0.3s ease-out, bottom 0.3s ease-out;
+      }
+    }
+
+    &__container {
+      display: flex;
+      flex-direction: column;
+      width: 35vw;
+      margin: 0 auto;
+      background-color: #fff;
+      border-radius: 8px;
+      box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+      transition: all .3s ease;
+      font-family: Helvetica, Arial, sans-serif;
+      padding: 0.5rem 1rem;
+
+      @media (max-width: 1023px) {
+        width: 100%;
+        height: auto;
+        border-radius: 0.75rem 0.75rem 0 0;
+      }
+    }
+
+    &__header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 0.5rem 0.25rem 0 1rem;
+
+      &__title {
+        padding: 0.3rem 0;
+        margin: 0;
+        text-align: left;
+        font-weight: bold;
+        font-size: 1em;
+      }
+    }
+
+    &__body {
+      min-height: 25vh;
+      max-height: 40vh;
+      overflow-y: auto;
+      overflow-x: hidden;
+      padding-right: 0.5rem;
+      margin: 0.25rem 0.25rem 1rem 0.25rem;
+
+      text-align: left;
+
+      &-author {
+        margin-left: 0.75rem;
+        margin-bottom: 0.75rem;
+      }
+
+      &__empty-list {
+        margin-top: 0.75rem;
+
+        button {
+          margin-top: 0.5rem;
+        }
+      }
+
+      &__file-version {
+        &__content {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+
+          &-info {
+            display: flex;
+            align-items: center;
+
+            &-number {
+              margin-right: 0.75rem;
+            }
+          }
+
+          &-link {
+            display: flex;
+            align-items: center;
+          }
+        }
+      }
+    }
+
+    &__footer {
+      margin-bottom: 0.5rem;
+      text-align: left;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+
+      &__button {
+        margin: 0.25rem;
+      }
+
+      @media (max-width: 1023px) {
+        margin-bottom: 1rem;
+        display: flex;
+        flex-direction: column-reverse;
+      }
+    }
+
+    &__close {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      padding: 0.5rem 0 0.5rem 0.5rem;
+      cursor: pointer;
+      transition: all .2s ease;
+
+      &:hover {
+        opacity: 0.8;
+      }
+      &:active {
+        opacity: 0.8;
+      }
+    }
+  }
+
+  .modal-leave-active {
+    transition: all .3s ease;
+  }
+  .modal-leave-to {
+    opacity: 0;
+  }
+
+  .fade-enter-active {
+    animation: go .3s;
+  }
+
+  @keyframes go {
+    from {
+      opacity: 0.5;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+</style>
---
+++ src/components/modals/js/modal-file-version.js
@@ -0,0 +1,105 @@
+import { mapGetters, mapActions } from 'vuex'
+import ListItem from '@/components/list/ListItem'
+
+export default {
+  name: 'modal-file-version',
+  components: {
+    ListItem
+  },
+  data () {
+    return {
+      fileDetail: {},
+      isLoading: false,
+      isUploading: false,
+      disableUpload: false
+    }
+  },
+  props: [ 'id' ],
+  computed: {
+    ...mapGetters([
+      'accessList',
+      'currentUser'
+    ]),
+    author () {
+      return (this.fileDetail && this.fileDetail.author && this.fileDetail.author.name)
+    },
+    ownerOfTheFile () {
+      return (this.currentUser.id === (this.fileDetail && this.fileDetail.author && this.fileDetail.author.id))
+    }
+  },
+  created () {
+    this.initData()
+  },
+  methods: {
+    ...mapActions([
+      'downloadFile',
+      'getFileDetail',
+      'updateFile'
+    ]),
+    initData () {
+      this.isLoading = true
+      const data = {
+        parentId: this.$route.params.parentId,
+        id: this.id
+      }
+      this.getFileDetail({
+        data,
+        callback: this.successGetFileDetail,
+        fail: this.failGetFileDetail
+      })
+    },
+    successGetFileDetail (res) {
+      this.isLoading = false
+      this.fileDetail = res.content
+    },
+    failGetFileDetail () {
+      this.disableUpload = true
+      this.isLoading = false
+      this.fileDetail = {}
+      this.$toasted.error('Fail to get file detail, please try again')
+    },
+    close () {
+      this.$emit('close')
+    },
+    onFileChange (e) {
+      this.isUploading = true
+      this.file = e.target.files[0]
+      this.upload(this.file)
+    },
+    constructFormData (file, type) {
+      let data = JSON.stringify({
+        name: this.fileDetail.name,
+        type: type
+      })
+      let formData = new FormData()
+      formData.append('data', data)
+      formData.append('file', file)
+      return formData
+    },
+    upload (file) {
+      let data = {
+        parentId: this.$route.params.parentId,
+        id: this.fileDetail.id,
+        content: this.constructFormData(file, 'FILE')
+      }
+      let config = {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      }
+      this.updateFile({
+        data: data,
+        configuration: config,
+        callback: this.successUpdateFile,
+        fail: this.failUpdateFile
+      })
+    },
+    successUpdateFile () {
+      this.isUploading = false
+      this.$toasted.success('Successfully updated file')
+      this.initData()
+    },
+    failUpdateFile () {
+      this.isUploading = false
+      this.$toasted.error('Fail to update file, please try again')
+    }
+  }
+}
