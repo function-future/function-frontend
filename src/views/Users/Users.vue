4b04ba320b1d36cef829da55f62cc0f18225ae13
karnandohuang <karnandohuang@outlook.com>
2019-11-15 21:51:05 +0700

    added empty state to users page

--- src/views/Users/Users.vue
+++ src/views/Users/Users.vue
@@ -31,58 +31,74 @@
                   </div>
                 </div>
               </div>
-              <div class="users__container__tabs-content__list-wrapper" v-else>
-                <div class="columns is-multiline">
-                  <div class="column is-12"
-                       v-for="user in userList"
-                       :key="user.id">
-                    <UserListItem :imageUrl="user.avatar">
-                      <template #name>
-                        {{ user.name }}
-                      </template>
-                      <template #info>
-                        <div>{{ batch(user) }}</div>
-                        <div>{{ user.university }}</div>
-                      </template>
-                      <template #actions>
-                        <b-dropdown aria-role="list"
-                                    position="is-bottom-left"
-                                    @click.prevent.stop>
-                          <button class="button is-text" slot="trigger">
-                            <b-icon icon="ellipsis-v" size="is-small" class="icon"></b-icon>
-                          </button>
-                          <b-dropdown-item
-                            aria-role="listitem"
-                            @click="goToEditUser(user.id, user.role)">
+              <div class="users__container__tabs-content__list-wrapper"
+                   v-if="!isLoading">
+                <div v-if="!usersEmpty">
+                  <div class="columns is-multiline">
+                    <div class="column is-12"
+                         v-for="user in userList"
+                         :key="user.id">
+                      <UserListItem :imageUrl="user.avatar">
+                        <template #name>
+                          {{ user.name }}
+                        </template>
+                        <template #info>
+                          <div>{{ batch(user) }}</div>
+                          <div>{{ user.university }}</div>
+                        </template>
+                        <template #actions>
+                          <b-dropdown aria-role="list"
+                                      position="is-bottom-left"
+                                      @click.prevent.stop>
+                            <button class="button is-text" slot="trigger">
+                              <b-icon icon="ellipsis-v" size="is-small" class="icon"></b-icon>
+                            </button>
+                            <b-dropdown-item
+                              aria-role="listitem"
+                              @click="goToEditUser(user.id, user.role)">
                               <span class="icon-wrapper">
                                 <b-icon icon="edit" class="icon" size="is-small"></b-icon>
                                 Edit
                               </span>
-                          </b-dropdown-item>
-                          <b-dropdown-item
-                            aria-role="listitem"
-                            @click="openDeleteConfirmationModal(user.id)">
+                            </b-dropdown-item>
+                            <b-dropdown-item
+                              aria-role="listitem"
+                              @click="openDeleteConfirmationModal(user.id)">
                               <span class="icon-wrapper">
                                 <b-icon icon="trash-alt" class="icon" size="is-small"></b-icon>
                                 Delete
                               </span>
-                          </b-dropdown-item>
-                          <b-dropdown-item
-                            aria-role="listitem"
-                            class="is-hidden-desktop">
-                            <b-button type="is-light" expanded>Cancel</b-button>
-                          </b-dropdown-item>
-                        </b-dropdown>
-                      </template>
-                    </UserListItem>
+                            </b-dropdown-item>
+                            <b-dropdown-item
+                              aria-role="listitem"
+                              class="is-hidden-desktop">
+                              <b-button type="is-light" expanded>Cancel</b-button>
+                            </b-dropdown-item>
+                          </b-dropdown>
+                        </template>
+                      </UserListItem>
+                    </div>
                   </div>
+                </div>
+                <div v-if="usersEmpty && !failFetchUser">
+                  <EmptyState src="users">
+                    <template #title>
+                      Looks so empty!
+                    </template>
+                    <template #message>
+                      Start adding new user
+                    </template>
+                  </EmptyState>
+                </div>
+                <div v-if="usersEmpty && failFetchUser">
+                  <EmptyState src="error" :errorState="true"></EmptyState>
                 </div>
               </div>
             </div>
           </b-tab-item>
         </b-tabs>
       </div>
-      <div class="users__container__pagination-wrapper" v-if="!isLoading && userList.length">
+      <div class="users__container__pagination-wrapper" v-if="!isLoading && !usersEmpty">
         <b-pagination
           :total="paging.totalRecords"
           :current.sync="paging.page"
