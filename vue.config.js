e389a7b95ca143966e81ef65f2da6664b325c62c
Jonathan <jonathan016@binus.ac.id>
2020-02-01 12:22:45 +0700

    Optimized PWA configuration and icons

--- vue.config.js
+++ vue.config.js
@@ -123,6 +123,19 @@ module.exports = {
   pwa: {
     name: 'Function',
     themeColor: '#02AAF3',
+    msTileColor: '#02AAF3',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+    manifestOptions: {
+      backgroundColor: '#02AAF3'
+    },
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-150x150.png'
+    },
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       swSrc: 'src/service-worker.js',
