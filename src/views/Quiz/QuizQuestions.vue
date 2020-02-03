f2cf010a3e63d824b6742b2bb28e229510d26f56
David William <david_wk98@yahoo.com>
2020-02-03 16:45:04 +0700

    Added bottom margin to handle scroll issue

--- src/views/Quiz/QuizQuestions.vue
+++ src/views/Quiz/QuizQuestions.vue
@@ -11,65 +11,66 @@
         </div>
       </div>
     </div>
-    <b-steps
-      v-model="currentNumber"
-      :animated="true"
-      :has-navigation="true"
-      :icon-prev="prevIcon"
-      :icon-next="nextIcon">
-
-      <b-step-item v-for="(question, idx) in studentQuizQuestions"
-                   :key="question.number"
-                   :label="(question.number) + ''"
-                   :clickable="true" :type="{'is-primary': !!answers[idx], 'is-light': !answers[idx]}">
-        <h1 class="title has-text-centered" v-html="question.text"></h1>
-        <div class="columns is-mobile" v-for="(option) in question.options" :key="option.id">
-          <div class="column is-flex">
-            <b-radio-button class="quiz-questions__container__options" :native-value="option.id" v-model="answers[idx]">
-              <ListItem :minHeight="'50px'">
-                <template #title>{{ option.label }}</template>
-              </ListItem>
-            </b-radio-button>
+    <div class="quiz-questions__container__body">
+      <b-steps
+        v-model="currentNumber"
+        :animated="true"
+        :has-navigation="true"
+        :icon-prev="prevIcon"
+        :icon-next="nextIcon">
+        <b-step-item v-for="(question, idx) in studentQuizQuestions"
+                     :key="question.number"
+                     :label="(question.number) + ''"
+                     :clickable="true" :type="{'is-primary': !!answers[idx], 'is-light': !answers[idx]}">
+          <h1 class="title has-text-centered" v-html="question.text"></h1>
+          <div class="columns is-mobile" v-for="(option) in question.options" :key="option.id">
+            <div class="column is-flex">
+              <b-radio-button class="quiz-questions__container__options" :native-value="option.id" v-model="answers[idx]">
+                <ListItem :minHeight="'50px'">
+                  <template #title>{{ option.label }}</template>
+                </ListItem>
+              </b-radio-button>
+            </div>
           </div>
-        </div>
-      </b-step-item>
-      <template
-        v-if="customNavigation"
-        slot="navigation"
-        slot-scope="{previous, next}">
-        <div class="quiz-questions__container__actions">
-          <b-button
-            class="quiz-questions__container__actions-prev"
-            outlined
-            type="is-danger"
-            icon-pack="fas"
-            icon-left="backward"
-            :disabled="previous.disabled"
-            @click.prevent="previous.action">
-            Previous
-          </b-button>
-          <b-button
-            class="quiz-questions__container__actions-next"
-            outlined
-            type="is-success"
-            icon-pack="fas"
-            icon-right="forward"
-            :disabled="next.disabled"
-            @click.prevent="next.action" v-if="!next.disabled">
-            Next
-          </b-button>
-          <b-button
-            outlined
-            type="is-primary"
-            icon-pack="fas"
-            icon-right="check-circle" class="quiz-questions__container__actions-submit"
-            @click="submitQuiz"
-            :disabled="isSubmitting" v-if="!!next.disabled">
-            Submit
-          </b-button>
-        </div>
-      </template>
-    </b-steps>
+        </b-step-item>
+        <template
+          v-if="customNavigation"
+          slot="navigation"
+          slot-scope="{previous, next}">
+          <div class="quiz-questions__container__actions">
+            <b-button
+              class="quiz-questions__container__actions-prev"
+              outlined
+              type="is-danger"
+              icon-pack="fas"
+              icon-left="backward"
+              :disabled="previous.disabled"
+              @click.prevent="previous.action">
+              Previous
+            </b-button>
+            <b-button
+              class="quiz-questions__container__actions-next"
+              outlined
+              type="is-success"
+              icon-pack="fas"
+              icon-right="forward"
+              :disabled="next.disabled"
+              @click.prevent="next.action" v-if="!next.disabled">
+              Next
+            </b-button>
+            <b-button
+              outlined
+              type="is-primary"
+              icon-pack="fas"
+              icon-right="check-circle" class="quiz-questions__container__actions-submit"
+              @click="submitQuiz"
+              :disabled="isSubmitting" v-if="!!next.disabled">
+              Submit
+            </b-button>
+          </div>
+        </template>
+      </b-steps>
+    </div>
     <QuizModal v-if="showPointModal" :result="result" :trialsLeft="trialsLeft" @retry="restart" @finish="finish">
 
     </QuizModal>
@@ -88,6 +89,9 @@
         &-timer {
           margin: 0.5em 0 1em 0;
         }
+      }
+      &__body {
+        margin-bottom: 5rem;
       }
       &__options {
         width: 100%;
