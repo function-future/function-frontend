c5f6605b4045b2b8539e58406beb3c299269d162
Priagung Satyagama <priagungsatyagama@gmail.com>
2020-02-03 15:29:43 +0700

    bugfix reminder toast

--- src/views/Reminders/js/reminder-form.js
+++ src/views/Reminders/js/reminder-form.js
@@ -107,8 +107,10 @@ export default {
     errorHandler (err) {
       console.log(err)
       this.toast({
-        message: 'Something went wrong',
-        type: 'is-error'
+        data: {
+          message: 'Something went wrong',
+          type: 'is-error'
+        }
       })
     },
     addMemberHandler (member) {
