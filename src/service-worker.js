1ff2b3ec94a817d0a1beffdf717cf4d1119a3c14
Jonathan <jonathan016@binus.ac.id>
2020-02-06 02:10:13 +0700

    Registered precache js & css files caching strategy

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,6 +5,22 @@ if (workbox) {
 
   workbox.routing.registerNavigationRoute('/index.html')
 
+  // Precache needs to be handled since workbox-precache plugin does caching
+  // on precached asset with something similar to CacheFirst strategy, which
+  // will cause the web app not updating should new changes are deployed.
+  // See https://developers.google.com/web/tools/workbox/modules/workbox-precaching#serving_precached_responses
+  workbox.routing.registerRoute(
+    /\.(?:css|js)$/i,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'function-precaches',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxAgeSeconds: 24 * 60 * 60
+        })
+      ]
+    })
+  )
+
   workbox.routing.registerRoute(
     /\.(?:png|gif|jpg|jpeg|webp|svg)$/i,
     new workbox.strategies.CacheFirst({
