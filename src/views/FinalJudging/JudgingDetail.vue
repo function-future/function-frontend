eafc79d2d9c4227eaf96194237f92b9a7c0548b3
David William <david_wk98@yahoo.com>
2020-02-05 22:41:52 +0700

    Addressed judging scrolling issues

--- src/views/FinalJudging/JudgingDetail.vue
+++ src/views/FinalJudging/JudgingDetail.vue
@@ -1,51 +1,53 @@
 <template>
   <div class="auto-overflow-container">
-    <div class="judging-session__container">
-      <div class="judging-session__action">
-        <b-button rounded
-                  icon-left="pen"
-                  type="is-primary"
-                  @click="moveToEditPage"
-                  v-if="accessList.edit">
-          Edit
-        </b-button>
-        <b-button rounded
-                  icon-left="trash"
-                  type="is-danger"
-                  @click="showDeleteConfirmationModal = true"
-                  v-if="accessList.delete">
-          Delete
-        </b-button>
-      </div>
-      <div class="judging-session__detail">
-        <div class="judging-session__detail-header">
+    <div class="judging_container">
+      <div class="judging-session__container">
+        <div class="judging-session__action">
+          <b-button rounded
+                    icon-left="pen"
+                    type="is-primary"
+                    @click="moveToEditPage"
+                    v-if="accessList.edit">
+            Edit
+          </b-button>
+          <b-button rounded
+                    icon-left="trash"
+                    type="is-danger"
+                    @click="showDeleteConfirmationModal = true"
+                    v-if="accessList.delete">
+            Delete
+          </b-button>
+        </div>
+        <div class="judging-session__detail">
+          <div class="judging-session__detail-header">
           <span class="is-size-5 has-text-weight-bold">
             {{judging.name}}
           </span>
-        </div>
-        <div class="judging-session__detail-batch">
+          </div>
+          <div class="judging-session__detail-batch">
           <span class="is-size-7">
             Batch {{judging.batchCode}}
           </span>
-        </div>
-        <div class="judging-session__detail-description">
+          </div>
+          <div class="judging-session__detail-description">
           <span v-html="descriptionCompiledMarkdown">
           </span>
+          </div>
+        </div>
+        <div class="judging-session__content level">
+          <comparison-item class="level-item judging-session__content-item"
+                           v-for="student in judging.students"
+                           :key="student.id" :studentData="student">
+
+          </comparison-item>
         </div>
       </div>
-      <div class="judging-session__content level">
-        <comparison-item class="level-item judging-session__content-item"
-             v-for="student in judging.students"
-             :key="student.id" :studentData="student">
-
-        </comparison-item>
-      </div>
+      <modal-delete-confirmation v-if="showDeleteConfirmationModal"
+                                 @close="showDeleteConfirmationModal = false"
+                                 @clickDelete="deleteThisJudging">
+        <div slot="description">Are you sure you want to delete this judging session?</div>
+      </modal-delete-confirmation>
     </div>
-    <modal-delete-confirmation v-if="showDeleteConfirmationModal"
-                               @close="showDeleteConfirmationModal = false"
-                               @clickDelete="deleteThisJudging">
-      <div slot="description">Are you sure you want to delete this judging session?</div>
-    </modal-delete-confirmation>
   </div>
 </template>
 
@@ -55,13 +57,15 @@
 <style lang="scss" scoped>
   @import "@/assets/css/main.scss";
 
+  .judging_container {
+    @media only screen and (max-width: 1023px) {
+      margin-bottom: 10vh;
+    }
+  }
+
   .judging-session {
     &__container {
       padding: 1rem 1.25rem;
-
-      @media only screen and (max-width: 1023px) {
-        margin-bottom: 10vh;
-      }
     }
 
     &__action {
