cc6c22755c8f568cbe0f3dcdbb9426d255d1764d
Jonathan <jonathan016@binus.ac.id>
2020-02-08 14:21:38 +0700

    Try new change

--- src/views/Feeds/FeedsRevamp.vue
+++ src/views/Feeds/FeedsRevamp.vue
@@ -54,13 +54,13 @@
         <div class="column is-3 disable-selection" @click="goToPage('announcements')">
           <div class="card-content">
             <div><b-icon icon="bullhorn" class="menu-icon"></b-icon></div>
-            <div>Announcements Update</div>
+            <div>Announcements</div>
           </div>
         </div>
         <div class="column is-3 disable-selection" @click="goToPage('activityBlogs')">
           <div class="card-content">
             <div><b-icon icon="newspaper" class="menu-icon"></b-icon></div>
-            <div>Blogs Update</div>
+            <div>Blogs</div>
           </div>
         </div>
         <div class="column is-3 disable-selection" v-if="menuList.files" @click="goToPage('files')">
